25c4a5497074f4530a7ec7a9f9b943c5c01737f6
DiwakarJ <lokeshbsccomputerscience@gmail.com>
2026-03-12 00:32:59 +0530

    cors setup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const jobApplicationRoutes=require('./router/jobApplicationRoutes')
 const app=express();
 
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: "https://careeratextraatech.netlify.app/",
    credentials: true
 }))
 
